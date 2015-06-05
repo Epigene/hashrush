e4d1ee4cd06613ed1cd7ff94b7601db84d1dcf83
Epigene <augusts.bautra@gmail.com>
2015-06-05 15:49:25 +0300

    Now args can also be a single array

--- lib/hashrush.rb
+++ lib/hashrush.rb
@@ -3,15 +3,16 @@ module Hashrush
   class ::Hash
     def self.build_from_symbol_variables(binding, *args)
       hash = Hash.new
+      args = args.flatten if args.class == Array
       args.each do |arg|
         raise "Argments must all be symbols!" unless arg.class == Symbol
         hash[arg] = binding.eval("#{arg.to_s}")# if is_variable?(arg)
       end
       return hash
     end
-    
+
     def self.rush(binding, *args)
-      Hash.build_from_symbol_variables(binding, args)  
+      Hash.build_from_symbol_variables(binding, args)
     end
   end
 
--- spec/hashrush_spec.rb
+++ spec/hashrush_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe Hashrush do
-  it 'has a version number' do
-    expect(Hashrush::VERSION).not_to be nil
-  end
 
   describe "Hash.build_from_symbol_variables" do
     it 'Builds a hash from passed variables' do
@@ -11,7 +8,12 @@ describe Hashrush do
       b = "value two"
       expect(Hash.build_from_symbol_variables(binding, :a, :b)).to eq ({a: "value one", b: "value two"})
     end
-    it 'Raises if variables are not dfined' do
+    it 'Builds a hash from a passed array' do
+      a = "value one"
+      b = "value two"
+      expect(Hash.build_from_symbol_variables(binding, [:a, :b]) ).to eq ({a: "value one", b: "value two"})
+    end
+    it 'Raises if variables are not defined' do
       a = "value one"
       b = "value two"
       expect {Hash.build_from_symbol_variables(binding, :c, :d)}.to raise_error
@@ -23,11 +25,23 @@ describe Hashrush do
     end
   end
 
-  describe "is_variable?" do
-    xit 'Reports whether there is a variable named like the symbol' do
+  describe "Hash.rush" do
+    it 'Builds a hash from passed variables' do
       a = "value one"
       b = "value two"
-      expect(is_variable?(:a)).to eq true
+      expect(Hash.rush(binding, :a, :b)).to eq ({a: "value one", b: "value two"})
+    end
+    it 'Raises if variables are not defined' do
+      a = "value one"
+      b = "value two"
+      expect {Hash.rush(binding, :c, :d)}.to raise_error
+    end
+    it 'Raises if symbol variables are not symbols' do
+      a = "value one"
+      b = "value two"
+      expect {Hash.rush(binding, a, "nein!")}.to raise_error "Argments must all be symbols!"
     end
   end
+
+
 end
